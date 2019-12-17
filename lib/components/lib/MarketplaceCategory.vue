8ef5444300b81b5f69a20ca380cc8b60c88599b7
Broc Gailit <brocgailit@gmail.com>
2019-12-17 15:48:53 -0800

    Add marketplace category component - Tranlations - Flexibility to augment

---
+++ lib/components/lib/MarketplaceCategory.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>
+        {{ $t(category)}}
+    </span>
+</template>
+
+<i18n src="./lang.json" ></i18n>
+
+<script>
+export default {
+    name: 'MarketplaceCategory',
+    props: {
+        category: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
