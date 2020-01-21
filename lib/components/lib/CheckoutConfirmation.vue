39536cd67c18f861dac453bb9aefa94d58a389ac
Broc Gailit <brocgailit@gmail.com>
2020-01-20 18:38:11 -0800

    Add checkout confirmation messaging.

--- lib/components/lib/CheckoutConfirmation.vue
+++ lib/components/lib/CheckoutConfirmation.vue
@@ -1,60 +1,87 @@
 <template>
-    <div class="section is-large checkout-confirmation" v-if="booking">
-        <h1 class="title">{{$t('thank-you-order')}}</h1>
-        <p class="subtitle is-5">
-            {{$t('thank-you-order-no')}}
-            {{ booking.orderNumber }}
-        </p>
-        <checkout-order-summary :booking="booking" is-confirmation  />
-        <div class="confirmation-actions">
-            <b-button type="is-dark" outlined size="is-medium" @click="printConfirmation">
-                {{ $t('print') }}
-            </b-button>
-            <b-button type="is-success" size="is-medium" @click="$parent.close()">
-                {{ $t('done') }}
-            </b-button>
-        </div>
+  <div v-if="booking" class="section is-large checkout-confirmation">
+    <h1 class="title">{{ $t('thank-you-order') }}</h1>
+    <p class="subtitle is-5">
+      {{ $t('thank-you-order-no') }}
+      {{ booking.orderNumber }}
+    </p>
+    <p class="content has-text-centered is-medium">
+      {{ $t(booking.status) }}
+    </p>
+    <p class="content has-text-centered" v-if="company && company.phone">
+      {{ $t('booking-message') }}
+      {{ company.phone }}
+    </p>
+    <checkout-order-summary :booking="booking" is-confirmation />
+    <div class="confirmation-actions">
+      <b-button
+        type="is-dark"
+        outlined
+        size="is-medium"
+        @click="printConfirmation"
+      >
+        {{ $t('print') }}
+      </b-button>
+      <b-button type="is-success" size="is-medium" @click="$parent.close()">
+        {{ $t('done') }}
+      </b-button>
     </div>
+  </div>
 </template>
 
 <i18n src="./lang.json"></i18n>
 
 <script>
+import { createNamespacedHelpers } from 'vuex'
 import CheckoutOrderSummary from './CheckoutOrderSummary.vue'
+const { mapState } = createNamespacedHelpers('rezdy')
 export default {
-    name: 'CheckoutConfirmation',
-    components: {
-        CheckoutOrderSummary
-    },
-    props: {
-        booking: {
-            type: Object,
-            default: () => null
-        }
-    },
-    methods: {
-        printConfirmation() {
-            if (typeof window !== 'undefined') window.print();
-        }
+  name: 'CheckoutConfirmation',
+  components: {
+    CheckoutOrderSummary
+  },
+  props: {
+    booking: {
+      type: Object,
+      default: () => null
     }
+  },
+  data() {
+    return {
+      company: null
+    }
+  },
+  computed: {
+    ...mapState({
+      companyAlias: state => state.options.companyAlias
+    })
+  },
+  async mounted() {
+    const { company } = await this.$rezdy.getCompany(this.companyAlias)
+    this.company = company
+  },
+  methods: {
+    printConfirmation() {
+      if (typeof window !== 'undefined') window.print()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 .confirmation-actions {
-    display: flex;
-    justify-content: center;
-    margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
 }
 
 .confirmation-actions > .button {
-    margin: 4px;
-} 
+  margin: 4px;
+}
 
-@media print { 
-    .confirmation-actions {
-        display: none;
-    }
+@media print {
+  .confirmation-actions {
+    display: none;
+  }
 }
 </style>
